ab3db7d55b68f81d939d6f8d2ce65c967bfd03f1
Chan Nyein Tun <chanyeintun@gmail.com>
2022-07-19 15:19:42 +0630

    get method

--- src/index.js
+++ src/index.js
@@ -2,8 +2,22 @@ const express=require("express");
 
 const app=express();
 
+app.all("/",function(req,res,next){
+      console.log('middleware')
+      next();
+})
+
 app.get("/",function (req,res){
       res.send("Hello world");
+})
+
+app.get("/todos",function (req,res){
+      res.send(JSON.stringify([{title:"some text"}]));
+})
+
+app.get("/todos/:todoId",function (req,res){
+      const {todoId}=req.params;
+      res.send(JSON.stringify({id:todoId,title:"some text"}));
 })
 
 app.listen(3000,()=>{
