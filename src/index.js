c9be74c73c2729ad52cb3e24682a47016d0b2619
Chan Nyein Tun <chanyeintun@gmail.com>
2022-07-20 11:59:02 +0630

    used router

--- src/index.js
+++ src/index.js
@@ -1,23 +1,41 @@
 const express=require("express");
+const helloRouter=require("./helloRouter");
 
 const app=express();
 
 app.all("/",function(req,res,next){
-      console.log('middleware')
+      console.log('some test')
       next();
 })
 
+app.use("/hello",helloRouter);
+
+app.use((req,res,next)=>{
+      console.log('logger middleware');
+      next();
+})
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended:false}));
+
+
+
 app.get("/",function (req,res){
-      res.send("Hello world");
+      res.status(200).send("Hello world");
 })
 
 app.get("/todos",function (req,res){
-      res.send(JSON.stringify([{title:"some text"}]));
+      res.status(200).send(JSON.stringify([{title:"some text"}]));
 })
 
 app.get("/todos/:todoId",function (req,res){
       const {todoId}=req.params;
-      res.send(JSON.stringify({id:todoId,title:"some text"}));
+      res.status(200).send(JSON.stringify({id:todoId,title:"some text"}));
+})
+
+app.use((req,res,next)=>{
+      res.status(404).send("Not Found");
 })
 
 app.listen(3000,()=>{
